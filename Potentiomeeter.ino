121f6d1978a33639919ff3a9485cb23edc3383a4
purplemountain <cailinwin@gmail.com>
2019-03-26 00:18:01 -0700

    Update Potentiomeeter.ino

--- Potentiomeeter.ino
+++ Potentiomeeter.ino
@@ -24,5 +24,6 @@ void loop() {
   // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
   float voltage = sensorValue * (5.0 / 1023.0);
   // print out the value you read:
+  // voltage = voltage * 2;
   Serial.println(voltage);
 }
